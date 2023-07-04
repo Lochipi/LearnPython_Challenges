99a88a49b9d3ff9dcb8f1997de51029984e94e9e
Cornelius Emase <corneliuslochipi@gmail.com>
2023-07-04 23:55:57 +0300

    added numbers challenge

---
+++ numbers/challenge_1.py
@@ -0,0 +1,26 @@
+'''question
+Reverse an integer '''
+
+def reverse_num(num):
+    str_num = str(num)
+
+    if str_num[0] == '-':   #If the number is negative, reverse the string from index 1
+        reversed_str = '-' + str_num[:0:-1]
+    else:
+        reversed_str = str_num[::-1]
+
+    #converting back a string to a number 
+    reversed_str_num = int(reversed_str)
+
+    return reversed_str_num
+
+
+
+# test
+num1 = 12345
+reversed_num1 = reverse_num(num1)
+print("Reversed integer:", reversed_num1)
+
+num2 = -6789
+reversed_num2 = reverse_num(-6789)
+print("Reversed integer:", reversed_num2)
